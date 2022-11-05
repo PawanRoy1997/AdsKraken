afd5fe59a94d6789ea4405b3561d02d102a13946
Pawan Roy <pawanroy0066@gmail.com>
2022-11-05 20:12:12 +0530

    Initial Project

---
+++ app/src/main/java/com/pawanroy1997/adkraken/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.pawanroy1997.adkraken
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.pawanroy1997.adkraken.ui.theme.AdKrakenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AdKrakenTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AdKrakenTheme {
+        Greeting("Android")
+    }
+}
